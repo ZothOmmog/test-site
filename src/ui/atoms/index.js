93039abce8f025e932bd23f3c8b6ef9847e6daaf
Ghatanothoa <abpstd@gmail.com>
2020-07-05 15:25:08 +0500

    Added exports from atoms

--- src/ui/atoms/index.js
+++ src/ui/atoms/index.js
@@ -1,1 +1,7 @@
 export { NavLinkCustom } from './nav-link-custom';
+export { Logo } from './logo';
+export { ButtonColored } from './button-colored';
+export { ButtonExit } from './button-exit';
+export { InputForm } from './input-form';
+export { NewsBody } from './news-body';
+export { NewsItemWrapper } from './news-item-wrapper';
