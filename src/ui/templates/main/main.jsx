62cdfadcb98df61464504385e293f404d4ae39f5
Host <paklin.2020@gmail.com>
2020-07-05 18:17:01 +0500

    Added main-page template

--- src/ui/templates/main/main.jsx
+++ src/ui/templates/main/main.jsx
@@ -3,7 +3,9 @@ import './style.scss';
 
 export const MainPage = ({ Header, MainBody }) => (
     <div className="main-page">
-        <Header />
+        <div className="main-page__header">
+            <Header />
+        </div>
         <div className="main-page__body">
             <MainBody />
         </div>
--- src/ui/templates/main/story.stories.js
+++ src/ui/templates/main/story.stories.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { MainPage } from './main';
+import { MainBody } from '@ui/molecules';
+import { MainHeader } from '@ui/organisms';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default {
     title: 'MainPage',
     component: MainPage
 }
+
+export const WithoutAuth = () => (
+    <Router>
+        <MainPage
+            Header={MainHeader}
+            MainBody={MainBody}
+        />
+    </Router>
+);
